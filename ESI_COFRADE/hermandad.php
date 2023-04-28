454cd86c4dad805011205cc7c1c19c43e40d28a7
MadaoSurmanito <alejandro.sanzhuerta@alum.uca.es>
2023-04-28 13:03:27 +0200

    vaElSql

--- ESI_COFRADE/hermandad.php
+++ ESI_COFRADE/hermandad.php
@@ -10,7 +10,19 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
 </head>
 
-<body>
+<body class="hermandad">
+    <audio autoplay>
+        <source src="/music/marcha3.mp3" type="audio/mpeg">
+        tu navegador no es compatible con este audio
+    </audio>
+
+    <h1><img class="logo" src="/img/logo.jpg" alt="logo de mi web">
+        <p>ESI COFRADE
+        <p>
+    </h1>
+    <nav>
+        <a class="btn" href="http://localhost:3000/menu.php">Inicio</a>
+    </nav>
     <?php
     // Conexión a la base de datos Oracle
     $conn = oci_connect('System', 'MiCarmen', '//localhost:1521/XE');
@@ -27,45 +39,63 @@
         WHERE h.nombre LIKE '" . $nombre . "'";
     $stmt = oci_parse($conn, $query); // preparar la consulta SQL
     oci_execute($stmt); // ejecutar la consulta SQL
-    // Imprimir los datos de una hermandad
+    // Imprimir los datos de una hermandad en una tabla
+    echo "<table>";
     while ($row1 = oci_fetch_array($stmt, OCI_ASSOC + OCI_RETURN_NULLS)) {
-        echo "<h4>Nombre:</h4> " . $row1['NOMBRE'] . "<br/>";
-        echo "<h4>Año de fundación:</h4> " . $row1['ANIO_FUNDACION'] . "<br/>";
-        echo "<h4>Dia de Salida:</h4> " . $row1['DIA_SALIDA'] . "<br/>";
-        echo "<h4>Ciudad:</h4> " . $row1['CIUDAD'] . "<br/>";
+        echo "<tr><td><h4>Nombre:</h4></td><td>" . $row1['NOMBRE'] . "</td></tr>";
+        echo "<tr><td><h4>Año de fundación:</h4></td><td>" . $row1['ANIO_FUNDACION'] . "</td></tr>";
+        echo "<tr><td><h4>Dia de Salida:</h4></td><td>" . $row1['DIA_SALIDA'] . "</td></tr>";
+        echo "<tr><td><h4>Ciudad:</h4></td><td>" . $row1['CIUDAD'] . "</td></tr>";
     }
+    echo "</table>";
     oci_free_statement($stmt); // Cerrar la conexión
+    
     // consulta SQL
-    $query =
-        "SELECT c.contenido as CURIOSIDAD
+    $query = "SELECT c.contenido as CURIOSIDAD
         FROM CURIOSIDADES c JOIN HERMANDADES h ON c.hermandad.nombre = h.nombre
         WHERE c.hermandad.nombre LIKE '" . $nombre . "'";
     $stmt = oci_parse($conn, $query); // preparar la consulta SQL
     oci_execute($stmt); // ejecutar la consulta SQL
-    // imprimir la lista de curiosidades
-    echo "<h3>CURIOSIDADES: </h3><ul>";
+    
+    // imprimir la lista de curiosidades en una tabla
+    
+    echo "<table>";
     $row = oci_fetch_array($stmt, OCI_ASSOC + OCI_RETURN_NULLS);
-    do {
-        if ($row == null)
-            echo "Vaya... no tenemos curiosidades para esta hermandad. Sé el primero en contarnos una:";
-        else
-            echo "<li>" . $row['CURIOSIDAD'] . "</li>";
-    } while (($row = oci_fetch_array($stmt, OCI_ASSOC + OCI_RETURN_NULLS)));
-    echo "</ul>";
+    echo "<tr><td><h4>Curiosidades</h4></td></tr>";
+    if ($row == null)
+        echo "<tr><td>Vaya... no tenemos curiosidades para esta hermandad. Sé el primero en contarnos una:</td></tr>";
+    else {
+        do {
+            echo "<tr><td>" . $row['CURIOSIDAD'] . "</td></tr>";
+        } while (($row = oci_fetch_array($stmt, OCI_ASSOC + OCI_RETURN_NULLS)));
+    }
+    echo "</table>";
+
     // Cerrar la conexión
     oci_free_statement($stmt);
     oci_close($conn);
     ?>
-    <h3>AÑADIR UNA CURIOSIDAD: </h3>
-    <form action="addCuriosidad.php" method="GET">
-        <input type="hidden" id="Hermandad" name="Hermandad" value="<?php echo $nombre ?>">
-        <input type="text" id="Curiosidad" name="Curiosidad">
-        <input type="submit" value="Enviar">
-    </form>
-    <form action="addHermano.php" method="GET">
-        <input type="hidden" id="Hermandad" name="Hermandad" value="<?php echo $nombre ?>">
-        <input type="submit" value="HAZTE HERMANO">
-    </form>
-    <button onclick="window.location.href='menu.php'">Volver</button>
+
+    <table>
+        <tr>
+            <td>
+                <h4>AÑADIR UNA CURIOSIDAD: </h4>
+                <form action="addCuriosidad.php" method="GET">
+                    <input type="hidden" id="Hermandad" name="Hermandad" value="<?php echo $nombre ?>">
+                    <input type="text" id="Curiosidad" name="Curiosidad">
+                    <input type="submit" value="Enviar">
+                </form>
+            </td>
+            <td>
+                <h4>HAZTE HERMANO: </h4>
+                <form action="addHermano.php" method="GET">
+                    <input type="hidden" id="Hermandad" name="Hermandad" value="<?php echo $nombre ?>">
+                    <input type="submit" value="HAZTE HERMANO">
+                </form>
+            </td>
+        </tr>
+    </table>
+
 </body>
+
 </html>
